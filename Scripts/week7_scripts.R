34a04ebb191c6da22bcd8a066a89d5c2c84714f4
Tyler Hoyt <watermaster@Tylers-MacBook-Pro.local>
2018-05-08 18:20:39 -0700

    Week 7

---
+++ Scripts/week7_scripts.R
@@ -0,0 +1,19 @@
+fish_data <- read.csv("Data/Gaeta_etal_CLC_data.csv")
+
+library(dplyr)
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 200, "big", "small"))
+
+#this creates a new table categorizing by fish size
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small"))
+
+#Get rid of scale lengths less than 1
+fish_data_cat = fish_data %>%
+  mutate(length_cat = ifelse(length > 300, "big", "small")) %>%
+  filter(scalelength > 1)
+
+#scatterplot with length on the x-axis and scalelength on the y-axis, and colored points for lake id
+library(tidyverse)
+ggplot(fish_data, aes(x = length, y = scalelength, color = lakeid)) +
+  geom_point()
